af742ff49dbc702617a73823c1f5f0ba4c139581
Manuel Odendahl <wesen@ruinwesen.com>
2026-04-01 18:42:42 -0400

    build(web): align storybook test helpers with 10.3

--- web/src/components/TranscriptViewer/stories/TranscriptViewer.stories.tsx
+++ web/src/components/TranscriptViewer/stories/TranscriptViewer.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
+import { fn } from "storybook/test";
 import Box from "@mui/material/Box";
 import { TranscriptViewer } from "../TranscriptViewer";
 import { withTheme } from "../../../test-utils/storybook-decorators";
